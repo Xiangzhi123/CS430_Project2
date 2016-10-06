c9f490ba66e01329804316ae7ded54d85f8de773
Xiangzhi Cao <xc42@nau.edu>
2016-10-06 02:05:58 -0700

    Update raycast.c

--- raycast.c
+++ raycast.c
@@ -22,15 +22,15 @@ typedef struct PPMimage {
 
 // return the square value of v
 static inline double sqr(double v) {
-  return v*v;
+  	return v*v;
 }
 
 // normalize the vector to a 3d unit vector
 static inline void normalize(double* v) {
-  double len = sqrt(sqr(v[0]) + sqr(v[1]) + sqr(v[2]));
-  v[0] /= len;
-  v[1] /= len;
-  v[2] /= len;
+ 	double len = sqrt(sqr(v[0]) + sqr(v[1]) + sqr(v[2]));
+	v[0] /= len;
+	v[1] /= len;
+  	v[2] /= len;
 }
 
 
@@ -83,51 +83,51 @@ int PPMWrite(char *outPPMVersion, char *outputFilename, PPMimage* buffer) {
 
 
 double sphereIntersection(double* Ro, double* Rd, double* Center, double r) {
-  // x = Rox + Rdx*t
-  // y = Roy + Rdy*t
-  // z = Roz + Rdz*t
-  // (x - Centerx)^2 + (y - Centery)^2 + (z - Centerz)^2 = r^2
-  //
-  // Then, (Rox + Rdx*t - Centerx)^2 + (Roy + Rdy*t - Centery)^2
-  // + (Roz + Rdz*t - Centery)^2 = r^2
-  //
-  // Then, Rox^2 + Rdx^2*t^2 + Centerx^2 + 2*Rox*Rdx*t - 2*Rox*Centerx - 2*Rdx*t*Centerx
-  // + Roy^2 + Rdy^2*t^2 + Centery^2 + 2*Roy*Rdy*t - 2*Roy*Centery - 2*Rdy*t*Centery
-  // + Roz^2 + Rdz^2*t^2 + Centerz^2 + 2*Roz*Rdz*t - 2*Roz*Centerz - 2*Rdz*t*Centerz
-  //
-  // Then, t^2(Rdx^2 + Rdy^2 + Rdz^2) +
-  // t*(2*Rox*Rdx + 2*TRoz*Rdz + 2*Roy*Rdy - 2*Rdx*Centerx - 2*Rdy*Centery - 2*Rdz*Centerz) +
-  // Rox^2 + Centerx^2 + Roy^2 + Centery^2 + Roz^2 + Centerz^2 -
-  // 2*Rox*Centerx - 2*Roy*Centery - 2*Roz*Centerz
-  // - r^2 = 0
-  double a = sqr(Rd[0]) + sqr(Rd[1]) + sqr(Rd[2]);
-  double b = 2*(Ro[0]*Rd[0] + Ro[1]*Rd[1] + Ro[2]*Rd[2] - Rd[0]*Center[0] - Rd[1]*Center[1] - Rd[2]*Center[2]);
-  double c = sqr(Ro[0]) + sqr(Ro[1]) + sqr(Ro[2]) + sqr(Center[0]) +
+  	// x = Rox + Rdx*t
+  	// y = Roy + Rdy*t
+  	// z = Roz + Rdz*t
+  	// (x - Centerx)^2 + (y - Centery)^2 + (z - Centerz)^2 = r^2
+  	//
+  	// Then, (Rox + Rdx*t - Centerx)^2 + (Roy + Rdy*t - Centery)^2
+  	// + (Roz + Rdz*t - Centery)^2 = r^2
+  	//
+  	// Then, Rox^2 + Rdx^2*t^2 + Centerx^2 + 2*Rox*Rdx*t - 2*Rox*Centerx - 2*Rdx*t*Centerx
+  	// + Roy^2 + Rdy^2*t^2 + Centery^2 + 2*Roy*Rdy*t - 2*Roy*Centery - 2*Rdy*t*Centery
+  	// + Roz^2 + Rdz^2*t^2 + Centerz^2 + 2*Roz*Rdz*t - 2*Roz*Centerz - 2*Rdz*t*Centerz
+  	//
+  	// Then, t^2(Rdx^2 + Rdy^2 + Rdz^2) +
+  	// t*(2*Rox*Rdx + 2*TRoz*Rdz + 2*Roy*Rdy - 2*Rdx*Centerx - 2*Rdy*Centery - 2*Rdz*Centerz) +
+  	// Rox^2 + Centerx^2 + Roy^2 + Centery^2 + Roz^2 + Centerz^2 -
+  	// 2*Rox*Centerx - 2*Roy*Centery - 2*Roz*Centerz
+  	// - r^2 = 0
+  	double a = sqr(Rd[0]) + sqr(Rd[1]) + sqr(Rd[2]);
+  	double b = 2*(Ro[0]*Rd[0] + Ro[1]*Rd[1] + Ro[2]*Rd[2] - Rd[0]*Center[0] - Rd[1]*Center[1] - Rd[2]*Center[2]);
+  	double c = sqr(Ro[0]) + sqr(Ro[1]) + sqr(Ro[2]) + sqr(Center[0]) +
              sqr(Center[1]) + sqr(Center[2]) - 2*(Ro[0]*Center[0]
              + Ro[1]*Center[1] + Ro[2]*Center[2]) - sqr(r);
-  double det = sqr(b) - 4*a*c;
-  if (det < 0) return -1;
-  det = sqrt(det);
-  double t0 = (-b - det) / (2*a);
-  double t1 = (-b + det) / (2*a);
+  	double det = sqr(b) - 4*a*c;
+  	if (det < 0) return -1;
+  	det = sqrt(det);
+  	double t0 = (-b - det) / (2*a);
+ 	double t1 = (-b + det) / (2*a);
 	if (t0 > 0) return t0;
-  if (t1 > 0) return t1;
+ 	if (t1 > 0) return t1;
 
-  return -1;
+  	return -1;
 }
 
 double planeIntersection(double* Ro, double* Rd, double* position, double* normal) {
-  // A(X0 + Xd * t) + B(Y0 + Yd * t) + (Z0 + Zd * t) + D = 0
-  // A(Rox + Rdx*t) + B(Roy + Rdy*t) + C(Roz + Rdz*t) + D = 0
-  // it could also be written as:
-  // A(Rox + Rdx*t - positionx) + B(Roy + Rdy*t - postiony) + C(Roz + Rdz*t - positionz) = 0
-  // t(A*Rdx + B*Rdy + C*Rdz) + (A*Rox + B*Roy + C*Roz - A*positionx - B*positiony - C*postionz) = 0
-  // t = - (A*Rox + B*Roy + C*Roz - A*positionx - B*positiony - C*positionz) / (A*Rdx + B*Rdy + C*Rdz)
-  double t = - (normal[0]*Ro[0] + normal[1]*Ro[1] + normal[2]*Ro[2] - normal[0]*position[0]
+  	// A(X0 + Xd * t) + B(Y0 + Yd * t) + (Z0 + Zd * t) + D = 0
+  	// A(Rox + Rdx*t) + B(Roy + Rdy*t) + C(Roz + Rdz*t) + D = 0
+  	// it could also be written as:
+  	// A(Rox + Rdx*t - positionx) + B(Roy + Rdy*t - postiony) + C(Roz + Rdz*t - positionz) = 0
+  	// t(A*Rdx + B*Rdy + C*Rdz) + (A*Rox + B*Roy + C*Roz - A*positionx - B*positiony - C*postionz) = 0
+  	// t = - (A*Rox + B*Roy + C*Roz - A*positionx - B*positiony - C*positionz) / (A*Rdx + B*Rdy + C*Rdz)
+  	double t = - (normal[0]*Ro[0] + normal[1]*Ro[1] + normal[2]*Ro[2] - normal[0]*position[0]
                 - normal[1]*position[1] - normal[2]*position[2]) / (normal[0]*Rd[0]
                 + normal[1]*Rd[1] + normal[2]*Rd[2]);
 
-  if (t > 0) return t;
+  	if (t > 0) return t;
 	return -1;
 }
 
