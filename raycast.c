f9faa863fb638e2cc7687493a9120590e6d0eb3f
Xiangzhi Cao <xc42@nau.edu>
2016-10-06 01:42:42 -0700

    Update raycast.c

--- raycast.c
+++ raycast.c
@@ -19,10 +19,13 @@ typedef struct PPMimage {
 	unsigned char *data;
 } PPMimage;
 
+
+// return the square value of v
 static inline double sqr(double v) {
   return v*v;
 }
 
+// normalize the vector to a 3d unit vector
 static inline void normalize(double* v) {
   double len = sqrt(sqr(v[0]) + sqr(v[1]) + sqr(v[2]));
   v[0] /= len;
@@ -46,7 +49,7 @@ int PPMDataWrite(char ppmVersionNum, FILE *outputFile, PPMimage* buffer) {
 		for (i = 0; i < buffer->height; i++) {
 			for (j = 0; j < buffer->width; j++) {
 				// similar thing as we did in reading body data for P3, but we use fprintf here to write data.
-				fprintf(outputFile, "%d %d %d ", buffer->data[i*buffer->width*3+j*3], buffer->data[i*buffer->width+j*3+1], buffer->data[i*buffer->width*3+2]);
+				fprintf(outputFile, "%d %d %d ", buffer->data[i*buffer->width*3+j*3], buffer->data[i*buffer->width*3+j*3+1], buffer->data[i*buffer->width*3+2]);
 			}
 			fprintf(outputFile, "\n");
 		}
@@ -101,14 +104,13 @@ double sphereIntersection(double* Ro, double* Rd, double* Center, double r) {
   double b = 2*(Ro[0]*Rd[0] + Ro[1]*Rd[1] + Ro[2]*Rd[2] - Rd[0]*Center[0] - Rd[1]*Center[1] - Rd[2]*Center[2]);
   double c = sqr(Ro[0]) + sqr(Ro[1]) + sqr(Ro[2]) + sqr(Center[0]) +
              sqr(Center[1]) + sqr(Center[2]) - 2*(Ro[0]*Center[0]
-             + Ro[1]*Center[1] + Ro[2]*Center[2])
-             - sqr(r);
+             + Ro[1]*Center[1] + Ro[2]*Center[2]) - sqr(r);
   double det = sqr(b) - 4*a*c;
   if (det < 0) return -1;
   det = sqrt(det);
   double t0 = (-b - det) / (2*a);
-  if (t0 > 0) return t0;
   double t1 = (-b + det) / (2*a);
+	if (t0 > 0) return t0;
   if (t1 > 0) return t1;
 
   return -1;
@@ -125,12 +127,12 @@ double planeIntersection(double* Ro, double* Rd, double* position, double* norma
                 - normal[1]*position[1] - normal[2]*position[2]) / (normal[0]*Rd[0]
                 + normal[1]*Rd[1] + normal[2]*Rd[2]);
 
-  if (t>0) return t;
+  if (t > 0) return t;
 	return -1;
 }
 
 int intersect(double* Rd, int objectNum, Object** objects) {
-	int closest = -1;
+	int closestObjectNum = -1;
 	double bestT = INFINITY;
 	int i;
 	double t;
@@ -139,9 +141,9 @@ int intersect(double* Rd, int objectNum, Object** objects) {
 		if (objects[i]->kind == 1) {
 			t = sphereIntersection(Ro, Rd, objects[i]->sphere.position, objects[i]->sphere.radius);
 			if (t) {
-				if (t > 0 && t < bestT) {
+				if (t > 0 && t <= bestT) {
 					bestT = t;
-					closest = i;
+					closestObjectNum = i;
 				}
 			}
 			else {
@@ -152,9 +154,9 @@ int intersect(double* Rd, int objectNum, Object** objects) {
 		else if (objects[i]->kind == 2) {
 			t = planeIntersection(Ro, Rd, objects[i]->plane.position, objects[i]->plane.normal);
 			if (t) {
-				if (t > 0 && t < bestT) {
+				if (t > 0 && t <= bestT) {
 					bestT = t;
-					closest = i;
+					closestObjectNum = i;
 				}
 			}
 			else {
@@ -163,7 +165,7 @@ int intersect(double* Rd, int objectNum, Object** objects) {
 			}
 		}
 	}
-	return closest;
+	return closestObjectNum;
 }
 
 PPMimage* rayCasting(char* filename, int w, int h, Object** objects) {
@@ -201,15 +203,14 @@ PPMimage* rayCasting(char* filename, int w, int h, Object** objects) {
 
 	double pixwidth = width / w;
 	double pixheight = height / h;
-
 	double pointx, pointy, pointz;
 	int j, k;
 	for (k = 0; k<h; k++) {
 		int count = (h-k-1)*w*3;
-		double pointy = -height / 2 + pixheight * (k + 0.5);
+		double vy = -height / 2 + pixheight * (k + 0.5);
 		for (j = 0; j<h; j++) {
-			double pointx = -width / 2 + pixwidth * (j + 0.5);
-			double Rd[3] = { pointx, pointy, 1 };
+			double vx = -width / 2 + pixwidth * (j + 0.5);
+			double Rd[3] = { vx, vy, 1 };
 
 			normalize(Rd);
 				int intersection = intersect(Rd, i, objects);
@@ -222,7 +223,6 @@ PPMimage* rayCasting(char* filename, int w, int h, Object** objects) {
 					pixel->r = 0;
 					pixel->g = 0;
 					pixel->b = 0;
-
 				}
 				buffer->data[count++] = pixel->r;
 				buffer->data[count++] = pixel->g;
@@ -238,11 +238,17 @@ int main(int argc, char **argv) {
 		fprintf(stderr, "Error: incorrect format('raycast width height input.json output.ppm')");
 		return (1);
 	}
+	char *name = argv[0];
 	char *w = argv[1];
 	char *h = argv[2];
 	char *inputFilename = argv[3];
 	char *outputFilename = argv[4];
 
+	if (strcmp(name, "raycast") != 0) {
+		fprintf(stderr, "Error: incorrect format, it has to be 'raycast'");
+		return (1);
+	}
+	if
 	Object** objects = malloc(sizeof(Object*) * 128);
 	int width = atoi(w);
 	int height = atoi(h);
